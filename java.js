f09f86587c666b349b6221bbc7c1e30e34bf8b02
Amanda <amanda_moller@estudante.sesisenai.org.br>
2025-04-04 08:35:33 -0300

    Add- chamando a função

--- java.js
+++ java.js
@@ -2,10 +2,12 @@
 //Questão 1;
 //somador de numeros;
 
-let n1 = parseFloat("Ditige um número");    //variável 1
-let n2 = parseFloat("Digite mais um número");    //variável 2
+let N1 = parseFloat("Ditige um número");    //variável 1
+let N2 = parseFloat("Digite mais um número");    //variável 2
 
 function somandoNumero(N1,N2){
 let sonma = (n1+n2);
  console.log('O resultado da soma é'+ somandoNumero)
 }
+ //chando função
+ somandoNumero(N1,N2)
