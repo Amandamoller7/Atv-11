d830aa61be1c46ac6df20e0a434f728afc3726aa
Amanda <amanda_moller@estudante.sesisenai.org.br>
2025-04-04 08:51:13 -0300

    add- repensando a 1

--- java.js
+++ java.js
@@ -2,12 +2,9 @@
 //Questão 1;
 //somador de numeros;
 
-let N1 = parseFloat("Ditige um número");    //variável 1
-let N2 = parseFloat("Digite mais um número");    //variável 2
-
-function somandoNumero(N1,N2){
-let sonma = (n1+n2);
- console.log('O resultado da soma é'+ somandoNumero)
+let N = parseFloat("Ditige um número");    //variável 1
+let i= parseInt('Quantos numeros você quer somar?')
+for (i){
+    let r=(r+N); 
 }
- //chando função
- somandoNumero(N1,N2)
+cosole.log(`A soma final é..'+ r); 
