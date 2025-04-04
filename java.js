94a5e2e0e755c4075bf743189372a7c826c3821f
Amanda <amanda_moller@estudante.sesisenai.org.br>
2025-04-04 08:32:14 -0300

    add- primeira questão

---
+++ java.js
@@ -0,0 +1,11 @@
+//atividade
+//Questão 1;
+//somador de numeros;
+
+let n1 = parseFloat("Ditige um número");    //variável 1
+let n2 = parseFloat("Digite mais um número");    //variável 2
+
+function somandoNumero(N1,N2){
+let sonma = (n1+n2);
+ console.log('O resultado da soma é'+ somandoNumero)
+}
